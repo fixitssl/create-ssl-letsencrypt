dac28bc65235af81a7e179650b9d766ab18b9210
fixitssl <107880364+fixitssl@users.noreply.github.com>
2022-06-20 14:44:51 -0400

    Update acme-TXT-status.sh

--- acme-TXT-status.sh
+++ acme-TXT-status.sh
@@ -1,1 +1,4 @@
-watch -n 5 host -t TXT _acme-challenge.$1
+#Type thename of the apex domain to run this command
+#It will status the DNS every 5 seconds for a change
+#If there is a change the watch command will automatically end (-g otion)
+watch -n 5 -g host -t TXT _acme-challenge.$1
